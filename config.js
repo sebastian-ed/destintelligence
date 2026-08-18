9bb322507d78dce6fce5ef6ed93e6fefb92be06e
sebastian-ed <seba020694@gmail.com>
2026-08-17 21:28:19 -0300

    Fix api key

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 // Pegá los dos datos PÚBLICOS de tu proyecto. Nunca pongas la service_role aquí.
 window.DESTINTELLIGENCE_CONFIG = {
   SUPABASE_URL: "https://dnnlbqnppxlmstneawrw.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_OWPXIPBPd4X0D3I8pFNxwA_Ygyfg5hJ"
+  SUPABASE_ANON_KEY: "sb_publishable_TmEcf7fCMBEHF1nXHxXpOw_cwX62_80"
 };
